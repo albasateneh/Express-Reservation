5b50ca80511010466df8c1a84dde744ca6476886
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-01-10 11:35:35 -0800

    added file structure and HTML Pages. Also created basic server

---
+++ Solved/server.js
@@ -0,0 +1,39 @@
+// Dependencies
+// =========================================================================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =========================================================================================================
+var app = express();
+var PORT = 3000;
+
+// Data Parsing
+//==============================================================================================================
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+})
