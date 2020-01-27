0a65e8854478833fe24e5090c6cdd5b4d408c658
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-01-27 12:26:26 -0800

    need to double check reservation form data

--- Solved/routes/apiRoutes.js
+++ Solved/routes/apiRoutes.js
@@ -10,4 +10,13 @@ module.exports = function (app) {
     app.get('/api/reservations', function(req, res) {
         res.json(waitingListData);
     })
+    app.post('/api/tables', function(req, res) {
+        if(tableData.length < 5) {
+            tableData.push(req.body);
+            res.json(true);
+        } else {
+            waitingListData.push(req.body);
+            res.json(false);
+        }
+    })
 }
