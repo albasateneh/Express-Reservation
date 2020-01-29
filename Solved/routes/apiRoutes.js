63ea1b45aa0330618b936c7167ef2163cbb67172
Sammy Albasateneh <salbasateneh@lazydogrestaurants.com>
2020-01-29 09:26:27 -0800

    created clear table post route

--- Solved/routes/apiRoutes.js
+++ Solved/routes/apiRoutes.js
@@ -6,10 +6,10 @@ var waitingListData = require ('../data/waitinglistData')
 module.exports = function (app) {
     app.get('/api/tables', function(req, res){
         res.json(tableData);
-    })
+    });
     app.get('/api/reservations', function(req, res) {
         res.json(waitingListData);
-    })
+    });
     app.post('/api/tables', function(req, res) {
         if(tableData.length < 5) {
             tableData.push(req.body);
@@ -18,5 +18,12 @@ module.exports = function (app) {
             waitingListData.push(req.body);
             res.json(false);
         }
+    });
+    app.post('/api/clear', function() {
+        tableData = [];
+        waitingListData = [];
+
+        console.log(tableData);
+        console.log(waitingListData);
     })
 }
